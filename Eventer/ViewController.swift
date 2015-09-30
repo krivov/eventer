b846f2e279bafad4fee65d5aa98e273e2f944d55
Sergey Krivov <sergey@krivov.info>
2015-09-30 23:50:14 +0300

    init

---
+++ Eventer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Eventer
+//
+//  Created by Sergey Krivov on 30.09.15.
+//  Copyright © 2015 Sergey Krivov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
