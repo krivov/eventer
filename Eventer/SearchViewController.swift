e19553a255dd1b60ac293a63f1a859058d0907d8
Sergey Krivov <sergey@krivov.info>
2015-10-01 00:40:55 +0300

    skeleton with controllers

--- Eventer/SearchViewController.swift
+++ Eventer/SearchViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  SearchViewController.swift
 //  Eventer
 //
 //  Created by Sergey Krivov on 30.09.15.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class SearchViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
